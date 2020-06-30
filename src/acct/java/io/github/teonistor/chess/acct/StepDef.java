e74074b3b1cc0831398deb86157fafa6505d0482
Teo G Nistor <teo.g.nistor@gmail.com>
2020-06-30 23:00:53 +0100

    chess: WIP Acceptance tests

--- src/acct/java/io/github/teonistor/chess/acct/StepDef.java
+++ src/acct/java/io/github/teonistor/chess/acct/StepDef.java
@@ -2,34 +2,67 @@ package io.github.teonistor.chess.acct;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java8.En;
+import io.github.teonistor.chess.board.Position;
 import io.github.teonistor.chess.core.Game;
 import io.github.teonistor.chess.core.GameFactory;
 import io.github.teonistor.chess.core.Player;
 import io.github.teonistor.chess.inter.Input;
 import io.github.teonistor.chess.inter.TerminalView;
+import org.mockito.InOrder;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 
 public class StepDef implements En {
 
     private final GameFactory gameFactory = new GameFactory();
     private final Input white = mock(Input.class);
     private final Input black = mock(Input.class);
+    private final TerminalView view = spy(new TerminalView());
+    private final InOrder viewOrder = inOrder(view);
 
     private Game game;
 
     public StepDef() {
+        System.out.println("** new StepDef() **");
 
         Given("a game in its initial state", () -> {
-            game = gameFactory.createGame(() -> white, () -> black, new TerminalView());
+            game = gameFactory.createGame(() -> white, () -> black, view);
         });
 
-        When("the following moves take place", (final DataTable data) -> {
+        When("the following moves take place", (final DataTable dataTable) -> {
+            final List<List<String>> data = dataTable.asLists();
 
+            final Iterator<List<String>> dataIterator = data.iterator();
+            when(white.takeInput(any(), any())).then(invocation -> {
+                final List<String> nextData = dataIterator.next();
+                assertThat(nextData.get(0)).isEqualTo("White");
+                return invocation.getArgument(1, BiFunction.class).apply(Position.valueOf(nextData.get(1)), Position.valueOf(nextData.get(2)));
+            });
+            when(black.takeInput(any(), any())).then(invocation -> {
+                final List<String> nextData = dataIterator.next();
+                assertThat(nextData.get(0)).isEqualTo("Black");
+                return invocation.getArgument(1, BiFunction.class).apply(Position.valueOf(nextData.get(1)), Position.valueOf(nextData.get(2)));
+            });
+
+            game.play();
+
+            for (int i = 0; i < data.size(); i++) {
+                viewOrder.verify(view).announce(String.format("%s moves: %s - %s", data.get(i).get(0), data.get(i).get(1), data.get(i).get(2)));
+            }
         });
 
         Then("{} wins", (Player player) -> {
-            System.out.println(player);
+            viewOrder.verify(view).announce(String.format("%s wins", player));
         });
     }
 }
--- src/acct/resources/GameExample.feature
+++ src/acct/resources/GameExample.feature
@@ -3,7 +3,16 @@ Feature: Example games from history/trivia
   Scenario: Fool
     Given a game in its initial state
     When the following moves take place
-    # The player is an implicit assertion. The move choice is an input. The capture is an explicit assertion
-      | a | b |
+      | White | D2 | D4 | false |
+      | Black | F7 | F5 | false |
+      | White | C1 | G5 | false |
+      | Black | H7 | H6 | false |
+      | White | G5 | H4 | false |
+      | Black | G7 | G5 | false |
+      | White | H4 | G3 | false |
+      | Black | F5 | F4 | false |
+      | White | E2 | E3 | false |
+      | Black | F4 | G3 | false |
+      | White | D1 | H5 | false |
 
     Then White wins
