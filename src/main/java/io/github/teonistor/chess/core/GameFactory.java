b1812823802a940335549591153b52e1986c0715
Teo G Nistor <teo.g.nistor@gmail.com>
2020-10-06 22:42:37 +0100

    Hardly anything left of GameFactory

--- src/main/java/io/github/teonistor/chess/core/GameFactory.java
+++ src/main/java/io/github/teonistor/chess/core/GameFactory.java
@@ -2,28 +2,11 @@ package io.github.teonistor.chess.core;
 
 import io.github.teonistor.chess.inter.Input;
 import io.github.teonistor.chess.inter.MultipleViewWrapper;
-import io.github.teonistor.chess.inter.TerminalInput;
-import io.github.teonistor.chess.inter.TerminalView;
 import io.github.teonistor.chess.inter.View;
-import io.vavr.collection.HashMap;
-import io.vavr.collection.Map;
-import org.apache.commons.lang3.Functions.FailableFunction;
 
 public class GameFactory {
 
-    private final Map<StateProvision, FailableFunction<String,GameStateProvider,Exception>> stateProvision;
-
-    public GameFactory() {
-        stateProvision = HashMap.of(
-                StateProvision.New,  ignore   -> World.initialStateProvider(),
-                StateProvision.Load, filename -> World.saveLoad().doLoadState(filename));
-    }
-
-    public Game createTerminalGame() {
-        return createGame(new TerminalInput(), new TerminalInput(), new TerminalView());
-    }
-
-    public Game createGame(Input white, Input black, View... views) {
-        return new Game(white.stateProvision(stateProvision), World.checkRule(), World.gameOverChecker(), white, black, MultipleViewWrapper.wrapIfNeeded(views));
+    public Game createGame(GameStateProvider stateProvider, Input white, Input black, View... views) {
+        return new Game(stateProvider, World.checkRule(), World.gameOverChecker(), white, black, MultipleViewWrapper.wrapIfNeeded(views));
     }
 }
--- src/test/java/io/github/teonistor/chess/core/GameFactoryTest.java
+++ src/test/java/io/github/teonistor/chess/core/GameFactoryTest.java
@@ -1,71 +1,38 @@
 package io.github.teonistor.chess.core;
 
 import io.github.teonistor.chess.inter.Input;
-import io.github.teonistor.chess.inter.TerminalInput;
-import io.github.teonistor.chess.inter.TerminalView;
 import io.github.teonistor.chess.inter.View;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.springframework.test.util.ReflectionTestUtils.getField;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 class GameFactoryTest {
 
     private static final GameFactory gameFactory = new GameFactory();
 
     @Test
-    void constructorInitialisedFields() {
-        final SoftAssertions soft = new SoftAssertions();
-        final Object underAttackRule = getField(gameFactory, "underAttackRule");
-        final Object checkRule = getField(gameFactory, "checkRule");
-        final Object gameOverChecker = getField(gameFactory, "gameOverChecker");
-        final Object initialBoardProvider = getField(gameFactory, "initialBoardProvider");
-        final Object initialStateProvider = getField(gameFactory, "initialStateProvider");
-
-        soft.assertThat(underAttackRule).isNotNull();
-        soft.assertThat(checkRule).isNotNull();
-        soft.assertThat(gameOverChecker).isNotNull();
-        soft.assertThat(initialBoardProvider).isNotNull();
-        soft.assertThat(initialStateProvider).isNotNull();
-        soft.assertThat(getField(checkRule, "underAttackRule")).isEqualTo(underAttackRule);
-        soft.assertThat(getField(gameOverChecker, "underAttackRule")).isEqualTo(underAttackRule);
-        soft.assertThat(getField(initialBoardProvider, "underAttackRule")).isEqualTo(underAttackRule);
-        soft.assertThat(getField(initialStateProvider, "initialBoardProvider")).isEqualTo(initialBoardProvider);
-
-        soft.assertAll();
-    }
-
-    @Test
-    void createTerminalGame() {
-        final Game game = gameFactory.createTerminalGame();
-
-        assertThat((Input[]) getField(game, "inputs")).allSatisfy(input -> assertThat(input).isInstanceOf(TerminalInput.class));
-        assertThat(getField(game, "view")).isInstanceOf(TerminalView.class);
-    }
-
-    @Test
     void createGame() {
+        final GameStateProvider stateProvider = mock(GameStateProvider.class);
+        final CheckRule checkRule = mock(CheckRule.class);
+        final GameOverChecker gameOverChecker = mock(GameOverChecker.class);
         final Input inputOne = mock(Input.class);
         final Input inputTwo = mock(Input.class);
         final View viewOne = mock(View.class);
         final View viewTwo = mock(View.class);
         final View viewThree = mock(View.class);
 
-        final Game game = gameFactory.createGame(inputOne, inputTwo, viewOne, viewTwo, viewThree);
+        setField(World.class, "checkRule", checkRule);
+        setField(World.class, "gameOverChecker", gameOverChecker);
 
+        final Game game = gameFactory.createGame(stateProvider, inputOne, inputTwo, viewOne, viewTwo, viewThree);
+
+        assertThat(getField(game, "gameStateProvider")).isEqualTo(stateProvider);
+        assertThat(getField(game, "gameOverChecker")).isEqualTo(gameOverChecker);
+        assertThat(getField(game, "checkRule")).isEqualTo(checkRule);
         assertThat((Input[]) getField(game, "inputs")).containsExactly(inputOne, inputTwo);
         assertThat((Iterable) getField(getField(game, "view"), "views")).containsExactlyInAnyOrder(viewOne, viewTwo, viewThree);
-    }
-
-    private Object getFieldRec(Object object, String...names) {
-        return getFieldRec(object, 0, names);
-    }
-
-    private Object getFieldRec(Object object, int i, String...names) {
-        return i == names.length
-             ? object
-             : getFieldRec(getField(object, names[i]), i+1, names);
     }
 }
