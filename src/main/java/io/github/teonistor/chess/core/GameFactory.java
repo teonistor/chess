55f5d95568d594144172ec17d1cabda20f5cd4b6
Teo G Nistor <teo.g.nistor@gmail.com>
2020-10-14 23:24:21 +0100

    Piece box

--- src/main/java/io/github/teonistor/chess/core/GameFactory.java
+++ src/main/java/io/github/teonistor/chess/core/GameFactory.java
@@ -6,15 +6,16 @@ import io.github.teonistor.chess.inter.MultipleViewWrapper;
 import io.github.teonistor.chess.inter.TerminalInput;
 import io.github.teonistor.chess.inter.TerminalView;
 import io.github.teonistor.chess.inter.View;
+import io.github.teonistor.chess.piece.PieceBox;
 import io.vavr.collection.HashMap;
 import io.vavr.collection.Map;
-import io.github.teonistor.chess.core.StateProvision;
 
 public class GameFactory {
 
     private final UnderAttackRule underAttackRule;
     private final CheckRule checkRule;
     private final GameOverChecker gameOverChecker;
+    private final PieceBox pieceBox;
     private final InitialBoardProvider initialBoardProvider;
     private final InitialStateProvider initialStateProvider;
     private final Map<StateProvision, InitialStateProvider> stateProvision;
@@ -23,9 +24,10 @@ public class GameFactory {
         underAttackRule = new UnderAttackRule();
         checkRule = new CheckRule(underAttackRule);
         gameOverChecker = new GameOverChecker(underAttackRule);
-        initialBoardProvider = new InitialBoardProvider(underAttackRule);
+        pieceBox = new PieceBox(underAttackRule);
+        initialBoardProvider = new InitialBoardProvider(pieceBox);
         initialStateProvider = new InitialStateProvider(initialBoardProvider);
-        stateProvision = HashMap.of(New, initialStateProvider);
+        stateProvision = HashMap.of(StateProvision.New, initialStateProvider);
     }
 
     public Game createTerminalGame() {
