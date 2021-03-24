6f42d44bbeddcd2009d3ab268a3e40290dfd52d7
Teo G Nistor <teo.g.nistor@gmail.com>
2021-03-24 19:28:48 +0000

    Remove unused classes

--- src/main/java/io/github/teonistor/chess/core/GameFactory.java
+++
@@ -1,8 +0,0 @@
-package io.github.teonistor.chess.core;
-
-import io.github.teonistor.chess.inter.Input;
-
-// Isolates the parameters which the control loop is responsible for from those which the factory is responsible for
-public interface GameFactory {
-    Game create(GameStateProvider provider, Input input);
-}
